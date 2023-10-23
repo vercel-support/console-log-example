059f7d23f1b6b02fbad7df8c2db0f332d22ede4b
Riz <rizwana.akmal@hotmail.com>
2023-10-23 08:48:29 +0100

    dynamic

--- app/api/console-example/route.js
+++ app/api/console-example/route.js
@@ -1,4 +1,6 @@
 // import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
 export function GET() {
   console.log("Logged to console");
   return Response.json({ text: "Hello" });
